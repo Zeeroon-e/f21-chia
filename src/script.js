1b0f02f70950ae7ea80f03718b9d8b21932d082b
Zeeroon-e <chia.kmohamad@hotmail.com>
2022-08-26 11:39:06 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/index.html 	modified:   src/script.js 	modified:   src/style/style.scss

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
-console.log("success");
+const btn = document.getElementById("btn-1");
+const dialogrutan = document.getElementsByClassName("dialogruta");
+
+
+btn.addEventListener("click", function () {
+    console.log("clicked");
+    hideBox();
+    
+});
+
+const hideBox = () => {
+    dialogrutan[0].style.display = 'none'
+}
